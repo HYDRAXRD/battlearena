47354cffa5f1775c7731c543f8287b5d436389c6
HYDRA <hydrxrd@gmail.com>
2026-02-20 23:38:05 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react ';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useGameState } from '@/game/useGameState';
 import { ENEMIES, SHOP_ITEMS } from '@/game/constants';
